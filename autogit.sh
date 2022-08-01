484bccbe9dfce036dff8774a779b8d4dd663428c
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-07-31 22:46:43 -0300

    Next step: Overloading a Function

--- autogit.sh
+++ autogit.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -m "Next step: Ref in Functions (Warning!)"
+git commit -m "Next step: Overloading a Function"
 git push -u origin main
 git status
 
-# next link: https://www.w3schools.com/cpp/cpp_function_reference.asp
+# next link: https://www.w3schools.com/cpp/cpp_function_overloading.asp
--- functions/main.cpp
+++ functions/main.cpp
@@ -24,11 +24,36 @@ void funcToCalc(float num1, float num2)
     cout << "Num2 = " << cbrt(num2) << endl;
 }
 
+void exchangeValues(int &x, int &y)
+{
+    int z = x; // store the 'x' content
+    x = y;
+    y = z;
+    // Now, 'x' and 'y' exchange yours values!
+}
+
 int main()
 {
-    cout << "\nCalling the funcToCalc():\n";
+    int num01 = 10;
+    int num02 = 15;
 
-    funcToCalc(126.85, 43.45);
+    // cout << "\nCalling the funcToCalc():\n";
+
+    // funcToCalc(126.85, 43.45);
+
+    /*
+        You can also pass a reference to the function. This can be useful when you need to change the value of the arguments.
+    */
+
+    cout << "\nNormally, we have:" << endl;
+
+    cout << num01 << " and " << num02 << endl;
+
+    cout << "\nCalling the exchangeValues():\n";
+
+    exchangeValues(num01, num02);
+
+    cout << num01 << " and " << num02 << endl;
 
     return 0;
 }
