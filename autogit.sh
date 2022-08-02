2e8e044ac7c750b659e17da3449927b3f5301e7f
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-08-02 19:25:51 -0300

    Next step: C++ Inheritance

--- autogit.sh
+++ autogit.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -m "Next step: C++ Encapsulament"
+git commit -m "Next step: C++ Inheritance"
 git push
 git status
 
-# next link: https://www.w3schools.com/cpp/cpp_encapsulation.asp
+# next link: https://www.w3schools.com/cpp/cpp_inheritance.asp
--- classes/part05Encap.cpp
+++ classes/part05Encap.cpp
@@ -2,8 +2,49 @@
 
 using namespace std;
 
+class Employee {
+    private:
+        float salary;
+
+    public:
+        void setSalary() {
+            cout << "\nInform your gross salary: ";
+            cin >> salary;
+            salary = salary - (salary * 0.15);
+        }
+
+        void getSalary() {
+            cout << "\nYour net salary is R$" << salary << "\n" << endl;
+        }
+};
+
 int main() {
-    /* Encapsulation in C++ */
+    /*
+        Encapsulation in C++ 
+
+        The meaning of Encapsulation, is to make sure that "sensitive" data is hidden from users. To achieve this, you must declare class variables/attributes as private.
+    */
+
+    /*
+        Access Private Members:
+        
+        To access a private attribute, use public "get" and "set" methods.
+    */
+
+    Employee emp;
+
+    emp.setSalary();
+    emp.getSalary();
     
     return 0;
+
+    /*
+        Why Encapsulation?
+        
+        1 - It is considered good practice to declare your class attributes as private (as often as you can). 
+        
+        2 - Encapsulation ensures better control of your data, because you (or others) can change one part of the code without affecting other parts.
+        
+        3 - Increased security of data.
+    */
 }
