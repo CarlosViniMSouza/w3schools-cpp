205819d0176f09fd29766a19a22caed20eddaf64
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-08-02 21:21:18 -0300

    Next step: C++ Polymorphism

--- autogit.sh
+++ autogit.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -m "Next step: C++ Inheritance"
+git commit -m "Next step: C++ Polymorphism"
 git push
 git status
 
-# next link: https://www.w3schools.com/cpp/cpp_inheritance.asp
+# next link: https://www.w3schools.com/cpp/cpp_polymorphism.asp
--- classes/part05Encap.cpp
+++ classes/part05Encap.cpp
@@ -18,6 +18,25 @@ class Employee {
         }
 };
 
+class Car {
+    public:
+        string model = "Hylux S500";
+        float price = 250000.9;
+        int code = 2025001001;
+
+        void showCar() {
+            cout << "\nCode Car: " << code << endl;
+            cout << "Model Car: " << model << endl;
+            cout << "Price Car: R$" << price << endl;
+        }
+};
+
+// An derived class
+class fabCar: public Car {
+    public:
+        string brand = "Toyota";
+};
+
 int main() {
     /*
         Encapsulation in C++ 
@@ -31,13 +50,6 @@ int main() {
         To access a private attribute, use public "get" and "set" methods.
     */
 
-    Employee emp;
-
-    emp.setSalary();
-    emp.getSalary();
-    
-    return 0;
-
     /*
         Why Encapsulation?
         
@@ -46,5 +58,45 @@ int main() {
         2 - Encapsulation ensures better control of your data, because you (or others) can change one part of the code without affecting other parts.
         
         3 - Increased security of data.
+    */
+
+    Employee emp;
+
+    emp.setSalary();
+    emp.getSalary();
+
+    /*
+        Inheritance:
+
+        In C++, it is possible to inherit attributes and methods from one class to another. We group the "inheritance concept" into two categories:
+
+        derived class (child) - the class that inherits from another class base class (parent) - the class being inherited from
+        To inherit from a class, use the : symbol.
+    */
+
+    fflush;
+
+    fabCar myHylux;
+
+    myHylux.showCar();
+
+    cout << "\n" + myHylux.model + " is a original product from " + myHylux.brand << endl;
+
+    return 0;
+
+    /*
+        Multilevel Inheritance:
+
+        A class can also be derived from one class, which is already derived from another class.
+    */
+
+    /*
+        Multiple Inheritance:
+
+        A class can also be derived from more than one base class, using a ','. Example:
+
+
+        class MyChildClass: public MyClass, public MyOtherClass {
+        };
     */
 }
